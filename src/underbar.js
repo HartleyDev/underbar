db7743a3684cfbadcdf0b6b9ce154d2edc5a2107
TheDevilsKode <mrwebbin@gmail.com>
2014-04-25 17:07:39 -0700

    Finish _each rough draft

--- src/underbar.js
+++ src/underbar.js
@@ -42,7 +42,7 @@ var _ = {};
     var len = array.length;
     var last = len - 1;
     var start = len - n;
-    
+
     if(n > len){
       return array;
     }else
@@ -55,6 +55,15 @@ var _ = {};
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(var i = 0; i < collection.length; i++){
+        iterator(collection[i], i, collection);
+      }
+    }else if(typeof collection === 'object'){
+      for(var key in collection){
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
